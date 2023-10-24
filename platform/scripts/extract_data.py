9897a19621fec704772113508a07de5185458ce3
msoviche <maximilien.soviche@csgroup.eu>
2023-10-24 15:00:49 +0200

    [edit] update api key version

--- platform/scripts/extract_data.py
+++ platform/scripts/extract_data.py
@@ -42,6 +42,7 @@ def request_tmdb_api(request_url: str) -> dict:
     
     else:
         logger.critical(f"Error fetching data from TMDb: {response.status_code}")
+        logger.critical(response)
         return None
 
 
