913f8af17527cfc7b30fe0f9da3323c45d81b8c0
msoviche <maximilien.soviche@csgroup.eu>
2023-10-30 16:24:25 +0100

    [edit] terraform secret manager configuration

--- platform/terraform/secrets.tf
+++ platform/terraform/secrets.tf
@@ -15,3 +15,17 @@ resource "google_secret_manager_secret_version" "tmdb_api_key_secret_version" {
   secret_id  = google_secret_manager_secret.tmdb_api_key_secret.id
   secret_data = var.tmdb_api_key
 }
+
+resource "google_secret_manager_secret" "gcp_credentials_secret" {
+  secret_id = "GCP_CREDENTIALS"  # Replace with your desired secret name
+
+  replication {
+    automatic = true
+  }
+}
+
+resource "google_secret_manager_secret_version" "gcp_credentials_secret_version" {
+  secret_id  = google_secret_manager_secret.gcp_credentials_secret.id
+  secret_data = var.gcp_credentials
+}
+
--- platform/terraform/terraform.tfvars
+++ platform/terraform/terraform.tfvars
@@ -2,3 +2,4 @@ project_id = "movies-data-platform"
 region = "us-central1"
 dataset_name = "tmdb_dataset"
 tmdb_api_key = var.TMDB_API_KEY
+gcp_credentials = var.GCP_CREDENTIALS
